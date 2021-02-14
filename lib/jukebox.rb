e9642f93a07df32f2ef212e53f22f6579a7e59ed
Diana Liao <dijali@gmail.com>
2021-02-14 23:38:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -2,11 +2,11 @@
 
 #def say_hello(name)
 #  "Hi #{name}!"
-end
+#end
 
-puts "Enter your name:"
-users_name = gets.strip
+#puts "Enter your name:"
+#users_name = gets.strip
 
-puts say_hello(users_name)
+#puts say_hello(users_name)
 
 
