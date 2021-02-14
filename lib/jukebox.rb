905823f06614bd30637e6c2598c3460d15501adb
Diana Liao <dijali@gmail.com>
2021-02-14 23:48:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,12 +24,21 @@ songs = [
 def help 
   puts "I accept the following commands:"
   puts "- help : displays this help message" 
-  puts  "- list: displays a list of songs you can play" 
-  puts  "- play: lets you choose a song to play" 
-  puts   "- exit: exits this program"
+  puts "- list: displays a list of songs you can play" 
+  puts "- play: lets you choose a song to play" 
+  puts "- exit: exits this program"
   
 end 
 
 def list(songs)
   songs.each do {|song| puts song}
 end
+
+def play
+  puts "Please enter a song name or number:"
+  selection = gets.strip
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
